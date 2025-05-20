7a58e76b0713ab5854ac4c2e1091f1fde702186e
Lindsay <lindsay98@live.nl>
2025-05-20 14:31:45 +0200

    Job listings model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,54 +1,8 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array {
-        //return an array with job listings
-        return [
-            [
-                "id"=> "1",
-                "title" => "Director",
-                "salary" => "Є50.000",
-            ],
-            [
-                "id" => "2",
-                "title" => "Programmer",
-                "salary" => "Є10.000",
-            ],
-            [
-                "id" => "3",
-                "title" => "Teacher",
-                "salary" => "Є40.000",
-            ],
-            [
-                "id" => "4",
-                "title" => "Nurse",
-                "salary" => "Є34.000",
-            ],
-            [
-                "id" => "5",
-                "title" => "Physiotherapist",
-                "salary" => "Є60.000",
-            ],
-            [
-                "id" => "6",
-                "title" => "Data-analist",
-                "salary" => "Є70.000",
-            ],
-        ];
-
-    }
-
-    public static function find(int $id): array
-    {
-    // find a job by id, abort if not found
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
 }
