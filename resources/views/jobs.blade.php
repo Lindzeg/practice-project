ac0e7769f99d0c4aaa8295845f27468d3cfd947d
Lindsay <lindsay98@live.nl>
2025-05-30 15:09:58 +0200

    � Update jobs view layout and main CSS tweaks

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -58,55 +58,90 @@
                             {{--loop over first 3 vacancies--}}
                             @if(isset($vacancies))
                                 @foreach ($firstVacancies as $vacancy)
-                                    <li>
+                                    <li x-data="{ open : false}">
                                         <h3>{{$vacancy['title']}}</h3>
                                         <p>{{$vacancy['job_info']}}</p>
                                         <datetime>Posted at: {{$vacancy['created_at']}} <br> by: {{$vacancy->employer['company_name']}} </datetime>
+                                        <p id="open" x-on:click="open">See more</p>
+
+                                        <div class="expand" x-show="open">                                 
+                                            <h2>Vacancy title</h2>
+                                            <div class="heading">
+                                                <img id="logo" src="https://e7.pngegg.com/pngimages/779/61/png-clipart-logo-idea-cute-eagle-leaf-logo.png" alt="logo">
+                                                <button>Apply for the job</button>
+                                            </div>
+
+                                            <div class="vacancy-details">
+                                                <ul>
+                                                    @foreach ($vacancyDetails as $detail)
+                                                        <li class="row">
+                                                            <img src="{{ asset('storage/' . $detail->img_path) }}" alt="{{ $detail['job_details'] }}">
+                                                            <p>{{ $detail['vacancie_details'] }}</p>
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
+
+                                            </div>
+
+                                            <div class="function-discription">
+                                                <div class="text-container">
+                                                    <h2>About the function</h2>
+                                                    <p>{{$vacancy['about_job'] }} 
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt nibh nec mollis malesuada. </p>
+                                                </div>
+                                                <div class="text-container">
+                                                    <h2>Function discription</h2>
+                                                    <p>{{$vacancy['job_info']}}</p>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </li>
-                                <div class="expand">
-                                        <h2>Vacancy title</h2>
-                                        <div class="heading">
-                                            <img id="logo" src="{{ asset('storage/img/plcholder.png') }}" alt="logo">
-                                            <button>Apply for the job</button>
-                                        </div>
-
-                                        <div class="vacancy-details">
-                                            <ul>
-                                                @foreach ($vacancyDetails as $detail)
-                                                    <li class="row">
-                                                        <img src="{{ asset('storage/' . $detail->img_path) }}" alt="{{ $detail['job_details'] }}">
-                                                        <p>{{ $detail['vacancie_details'] }}</p>
-                                                    </li>
-                                                @endforeach
-                                            </ul>
-
-                                        </div>
-
-                                        <div class="function-discription">
-                                            <div class="text-container">
-                                                <h2>About the function</h2>
-                                                <p>{{$vacancy['about_job']}}</p>
-                                            </div>
-                                            <div class="text-container">
-                                                <h2>Function discription</h2>
-                                                <p>{{$vacancy['job_discription']}}</p>
-                                            </div>
-                                        </div>
-                                    </div>
+                                
                                 @endforeach
 
                                 {{-- When the button is pressed, open is set to true, and loops over all other available vacancies --}}
                                 @foreach ($otherVacancies as $vacancy)
                                   <template x-if="open">
-                                    <li>
+                                    <li x-data="{open: false}">
                                         <h3>{{$vacancy['title']}}</h3>
                                         <p>{{$vacancy['job_info']}}</p>
                                         <datetime>Posted at: {{$vacancy['created_at']}} <br> by: {{$vacancy->employer['company_name']}} </datetime>
+                                        <p>See more</p>
+
+                                        <div class="expand" x-show="open">                                 
+                                            <h2>Vacancy title</h2>
+                                            <div class="heading">
+                                                <img id="logo" src="https://e7.pngegg.com/pngimages/779/61/png-clipart-logo-idea-cute-eagle-leaf-logo.png" alt="logo">
+                                                <button>Apply for the job</button>
+                                            </div>
+
+                                            <div class="vacancy-details">
+                                                <ul>
+                                                    @foreach ($vacancyDetails as $detail)
+                                                        <li class="row">
+                                                            <img src="{{ asset('storage/' . $detail->img_path) }}" alt="{{ $detail['job_details'] }}">
+                                                            <p>{{ $detail['vacancie_details'] }}</p>
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
+
+                                            </div>
+
+                                            <div class="function-discription">
+                                                <div class="text-container">
+                                                    <h2>About the function</h2>
+                                                    <p>{{$vacancy['about_job'] }} 
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt nibh nec mollis malesuada. </p>
+                                                </div>
+                                                <div class="text-container">
+                                                    <h2>Function discription</h2>
+                                                    <p>{{$vacancy['job_info']}}</p>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </li>
                                   </template>
                                 @endforeach
-
-
 
                              @endif
                             {{--button shows remaining vacancies--}}
